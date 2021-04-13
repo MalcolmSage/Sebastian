e1bd5bb7ce05535ab61fa672ffbbed927bbd85ad
ThatLivelyLlama <thatlivelyllama@gmail.com>
2021-04-13 12:22:35 -0600

    testing bot token

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from .token from token 
 from discord.ext import commands
 
 client = commands.Bot(command_prefix = '.')
@@ -7,6 +8,6 @@ client = commands.Bot(command_prefix = '.')
 async def on_ready():
     print('You called?')
 
-client.run('ODMxNTg4NDI2NjM2OTg0MzMx.YHXbEw.KP3qw-KCEXWC7330Wa6acergJgc')
+client.run(token)
 
 
