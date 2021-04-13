c496355c1e8ec954c8111b215b2f8b903960a147
ThatLivelyLlama <thatlivelyllama@gmail.com>
2021-04-13 14:04:12 -0600

    Bot connection test

--- bot.py
+++ bot.py
@@ -2,11 +2,24 @@ import discord
 from bot_token import token
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+client = commands.Bot(command_prefix = '*', intents = intents)
 
 @client.event
 async def on_ready():
-    print('You called?')
+    print('Sebas is ready.')
+
+@client.event
+async def on_message(message):
+    print('test')
+
+@client.event
+async def on_member_join(member):
+    print(f'{member} has joined a server.')
+
+@client.event
+async def on_member_remove(member):
+    print(f'{member} has left a server.')
 
 client.run(token)
 
