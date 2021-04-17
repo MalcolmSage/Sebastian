6d82ac135e7921a69d329b124c3d5324af9d56b2
ThatLivelyLlama <thatlivelyllama@gmail.com>
2021-04-16 20:37:37 -0600

    reorganized code to allow for cogs

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
-import discord
-from bot_token import token
+import discord, os, dotenv, random
 from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+token = os.getenv('token')
 
 intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
 client = commands.Bot(command_prefix = '*', intents = intents)
@@ -9,17 +12,20 @@ client = commands.Bot(command_prefix = '*', intents = intents)
 async def on_ready():
     print('Sebas is ready.')
 
-@client.event
-async def on_message(message):
-    print('test')
+# Cog setup
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send(f'I will be providing my {extension} skills.')
 
-@client.event
-async def on_member_join(member):
-    print(f'{member} has joined a server.')
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    await ctx.send(f'I shall refrain from using my {extension} skills.')
 
-@client.event
-async def on_member_remove(member):
-    print(f'{member} has left a server.')
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        client.load_extension(f'cogs.{filename[:-3]}')
 
 client.run(token)
 
